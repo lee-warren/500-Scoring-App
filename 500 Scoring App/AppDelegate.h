cef8ac05cff88ed7fce64f6acc58715223ed3af7
Lee Warren <leewarren@Lees-MBP.gateway>
2015-01-24 15:55:26 +1100

    Initial Commit

---
+++ 500 Scoring App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  500 Scoring App
+//
+//  Created by Lee Warren on 24/01/2015.
+//  Copyright (c) 2015 Lee Warren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 500 Scoring App/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  500 Scoring App
+//
+
+//  Copyright (c) 2015 Lee Warren. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
